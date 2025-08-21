47d687f205c4f6ffb649366bbb2f22be2e39de8b
Torikul-48 <torikulislam732978@gmail.com>
2025-08-21 18:44:23 +0600

    Modified from master

--- Calculator.java
+++ Calculator.java
@@ -3,4 +3,5 @@ public class Calculator {
     public int subtract(int a, int b) { return a - b; }
     public int multiply(int a, int b) { return a * b; }
     public int divide(int a, int b) { return b != 0 ? a / b : 0; }
+    // Modified from master
 }
