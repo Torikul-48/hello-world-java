189f37241bc600799313cdff0da4438a1ee2550b
Torikul-48 <torikulislam732978@gmail.com>
2025-08-21 18:40:41 +0600

    Modified from master branch

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
-    public int add(int a, int b) { return a + b; }
+    public int addition(int a, int b) { return a + b; }
     public int subtract(int a, int b) { return a - b; }
     public int multiply(int a, int b) { return a * b; }
     public int divide(int a, int b) { return b != 0 ? a / b : 0; }
